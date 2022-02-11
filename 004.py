3d79b91c415ee960372bbb876f28031c3fd4c241
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-10 21:07:54 -0300

    Código girando perfeitamente.

---
+++ 004.py
@@ -0,0 +1,4 @@
+valor1 = input("Digite um valor: ")
+valor2 = input("Digite outro valor: ")
+soma = valor1 + valor2
+print("A soma entre {} e {} é igual a ", {}.format(valor1, valor2, soma))
---
+++ 005.py
@@ -0,0 +1,4 @@
+numero = input("Digite um numero: ")
+n1 = numero -1
+n2 = numero +2
+print("Analizando o valor {}, seu antecessor é {} e o sucessor é {}".format(numero, n1, n2))
